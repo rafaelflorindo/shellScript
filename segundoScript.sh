bb1a0580ec40bf2127cf37f75e45e8b1aa488991
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-02-21 09:40:05 -0300

    Add files via upload
    Segundo Script

---
+++ segundoScript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "########################################"
+echo "Data Atual do Sistema"
+date
+echo "Voc^e esta lodago no sistema como: "
+whoami
+echo "Tempo de login no sistema: "
+uptime
+echo "########################################"
