a20673febcf7de907837e1dfeacbb853ddf6678b
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-02-21 09:41:18 -0300

    Update segundoScript.sh

--- segundoScript.sh
+++ segundoScript.sh
@@ -2,7 +2,7 @@
 echo "########################################"
 echo "Data Atual do Sistema"
 date
-echo "Voc^e esta lodago no sistema como: "
+echo "Você esta lodago no sistema como: "
 whoami
 echo "Tempo de login no sistema: "
 uptime
