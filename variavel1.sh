b18ca2b649ec5e310c95e53a408c8bd0d2673bd6
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2019-02-23 13:05:03 -0300

    Imprimindo variaveis

---
+++ variavel1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+nome = "Rafael Alves Florindo"
+idade=38
+
---
+++ variavel2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+nome="Rafael Alves Florindo"
+idade=38
+sexo=Masculino
+email=rafaelflorindo@hotmail.com
+linkedin=rafaelflorindo@hotmail.com
+
+echo "**************Dados cadastrais**************"
+echo "<< Dados Pesoais >>"
+echo "Nome = $nome"
+echo "Idade = $idade"
+echo "Sexo = $sexo"
+echo "<< Dados para contato >>"
+echo "email = $email"
+echo "Linkedin = $linkedin"
