cf82657777ff2d992946150f631e0048e71ecfc2
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2019-02-23 16:36:44 -0300

    imprimindo com chaves

--- variavel5.sh
+++ variavel5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash    
-$nome = "Rafael Alves Florindo"
-$ano = "2019"
-$versao = '1.1'
+nome="Rafael Alves Florindo"
+ano="2019"
+versao='1.1'
  
 echo “************Dados do script************”
 echo "Programador: $nome"
---
+++ variavel6.sh
@@ -0,0 +1,4 @@
+#!/bin/bash    
+carro=voyage
+ano=2012
+echo "O seu $carro foi construido em ${ano}"
