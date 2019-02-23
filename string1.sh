3a8d4b50fbd3fffd3570fc23032ac736669d93a2
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2019-02-23 17:54:30 -0300

    string

---
+++ string1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash    
+#script: string1.sh
+
+professor="Rafael Alves Florindo"
+echo "O nome do professor ${professor} possui ${#professor} caracteres, incluindo os espacos."
+
+quantidadeCaracteres=${#professor}
+echo "Valor armazenado na variavel quantidadeCaracteres = $quantidadeCaracteres"
