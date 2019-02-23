9453be8529122422579d11f48af722e777361d7e
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-02-23 13:25:29 -0300

    Create variavel5.sh

---
+++ variavel5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash    
+$nome = "Rafael Alves Florindo"
+$ano = "2019"
+$versao = '1.1'
+ 
+echo “************Dados do script************”
+echo "Programador: $nome"
+echo "Ano: $ano"
+echo "Versão deste script $versao"
+echo “***************************************”
+ 
