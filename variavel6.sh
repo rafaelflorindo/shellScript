99ccc4eda9d47236ecc63ac5d8146dc93b8fbce7
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2019-02-23 17:02:44 -0300

    imprimindo com chaves

--- variavel6.sh
+++ variavel6.sh
@@ -1,4 +1,5 @@
 #!/bin/bash    
+#script: variavel6.sh
 carro=voyage
 ano=2012
 echo "O seu $carro foi construido em ${ano}"
---
+++ variavelEscape.sh
@@ -0,0 +1,4 @@
+#!/bin/bash    
+#script: variavelEscape.sh
+user=$USER
+echo "Usuario logado \$user"
