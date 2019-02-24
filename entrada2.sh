29c965e9720fb8821ff3c665f3c08eb4620eaf82
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2019-02-24 20:22:16 -0300

    entrada

--- entrada2.sh
+++ entrada2.sh
@@ -2,13 +2,29 @@
 #script: entrada1.sh
 
 lsSequencial=$(ls)
-echo "LS = $lsSequencial"
+echo "Listagem simples sequencial = $lsSequencial"
 
 lsSequencialColuna=$(ls -C)
-echo "******LS Simples ******" 
+echo "Listagem simples por colunas" 
 echo "$lsSequencialColuna"
 
 lsDetalhadoColuna=$(ls -l)
-echo "******LS Detalhado ******"
-echo "$lsDetalhadoColuna | column -c :"
+echo "Listagem detalhada por colunas"
+echo "$lsDetalhadoColuna"
+
+Listagem simples sequencial = entrada1.sh
+entrada2.sh
+localizacao.sh
+mensagem1.sh
+mensagem2.sh
+
+Listagem simples por colunas
+entrada1.sh	mensagem4.sh	     segundoScript.sh  variavel2.sh
+entrada2.sh	mensagem5.sh	     string1.sh        variavel3.sh
+
+Listagem detalhada por colunas
+total 96
+-rwxrwxr-x 1 florindo florindo 815 fev 24 20:04 entrada1.sh
+-rwxr-xr-x 1 florindo florindo 290 fev 24 20:17 entrada2.sh
+-rw-r--r-- 1 florindo florindo  90 fev 23 11:33 localizacao.sh
 
