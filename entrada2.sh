070862c96d4c2cddd6f4d56b928bd1f0ec004fb9
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2019-02-24 20:33:06 -0300

    entrada

--- entrada2.sh
+++ entrada2.sh
@@ -12,19 +12,4 @@ lsDetalhadoColuna=$(ls -l)
 echo "Listagem detalhada por colunas"
 echo "$lsDetalhadoColuna"
 
-Listagem simples sequencial = entrada1.sh
-entrada2.sh
-localizacao.sh
-mensagem1.sh
-mensagem2.sh
-
-Listagem simples por colunas
-entrada1.sh	mensagem4.sh	     segundoScript.sh  variavel2.sh
-entrada2.sh	mensagem5.sh	     string1.sh        variavel3.sh
-
-Listagem detalhada por colunas
-total 96
--rwxrwxr-x 1 florindo florindo 815 fev 24 20:04 entrada1.sh
--rwxr-xr-x 1 florindo florindo 290 fev 24 20:17 entrada2.sh
--rw-r--r-- 1 florindo florindo  90 fev 23 11:33 localizacao.sh
 
---
+++ entrada3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#script: entrada1.sh
+
+infoSystem=`df -h`
+echo "Informacoes detalhadas do sistema"
+echo "$infoSystem"
+
+#Informacoes detalhadas do sistema
+#Sist.fichs      Tama  Ocup Livre Uso% Montado em
+#udev            464M     0  464M   0% /dev
+#tmpfs            99M  1,4M   98M   2% /run
+#/dev/sda1       9,8G  5,8G  3,5G  63% /
+#tmpfs           493M     0  493M   0% /dev/shm
+
