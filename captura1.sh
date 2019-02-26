e3069d450660855a462350f3e6089dd345a2a7ef
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-02-26 15:33:32 -0300

    Update captura1.sh

--- captura1.sh
+++ captura1.sh
@@ -14,3 +14,12 @@ echo "Nome: $nome"
 #****DADOS CADASTRAIS****
 #Nome: rafael
 
+#!/bin/bash
+#script: capturaLogin
+echo -e "Sistema de autenticação 1.0"
+read -p "Login: " login
+read -s -p "Password: " passwd
+
+echo "Seus dados foram armazenados com sucesso!!!"
+echo "Login: $login"
+echo "Senha: $passwd"
