cad05a9a7ae5b07c1f9f41ab92c443ab5b7278c7
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2019-03-06 09:44:42 -0300

    autenticar

--- mediaTrimestral.sh
+++ mediaTrimestral.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #script: mediaTrimestral.sh
-clear
+#clear
 echo -e "\nCalculo da media Aritmetica trimestral\n"
 
 read -p "Informe a nota do 1. Trimestre: " nota1
@@ -8,9 +8,9 @@ read -p "Informe a nota do 2. Trimestre: " nota2
 read -p "Informe a nota do 3. Trimestre: " nota3
 
 soma=$(echo "scale=2; $nota1+$nota2+$nota3" | bc -l)
-echo $soma
+echo "Soma = $soma"
 media=$(echo "scale=2; $soma/3" | bc -l | cut -c1)
-echo $media
+echo "Media = $media"
 
 if [ $media -ge 6 ]
 then
---
+++ menu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#script: menu.sh
+clear
+user=rafael
+password=123
+echo "###################################################"
+read -p "Informe o seu login: " usuario
+read -s -p "Informe a senha para [sistema tec]: " senha
+echo 
+
+if [ -z $usuario ]
+then 
+	echo "Login em Branco"
+	exit 1
+else
+	if [ -z $senha ]
+	then 
+		echo "Usuario e Login em Branco"
+		exit 1
+	else
+		camposPreenchidos=1
+	fi
+fi
+
+if [ $camposPreenchidos -eq 1 ]
+then
+	if [ $user = $usuario ]
+	then
+		if [ $password = $senha ]
+		then
+			echo "<<< Login Autenticado >>>"
+		else
+			echo "Senha nao confere com o sistema"
+		fi
+	else
+		echo "Usuario nao confere com o sistema"
+	fi
+else
+	echo "Campos nao prenchidos"
+	exit 1
+fi
