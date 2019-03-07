dd3c8de1047939bec81967dbb125dbdf0ffb73c0
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2019-03-06 21:06:19 -0300

    caso2

--- menu.sh
+++ menu.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
-#script: menu.sh
-#clear
-user=rafael
-password=123
-echo "###################################################"
-read -p "Informe o seu login: " usuario
-read -s -p "Informe a senha para [sistema tec]: " senha
-echo 
+#script: caso1.sh
+clear
+echo "#####################################"
+echo "# Escolha a opcao do menu           #"
+echo "# 1 - Listar o Diretorio corrente   #"
+echo "# 2 - Imprimir o nome do diretorio  #"
+echo "# 3 - Mostrar o usuario logado      #"
+echo "# 4 - Tempo de sessao ativa         #"
+echo "# 5 - Processos em execucao         #"
+echo "#####################################"
+read -p ": " opcao
 
-if [ -z $usuario ]
-then 
-	echo "Login em Branco"
-	exit 1
-else
-	if [ -z $senha ]
-	then 
-		echo "Usuario e Login em Branco"
-		exit 1
-	else
-		camposPreenchidos=1
-	fi
-fi
-
-if [ $camposPreenchidos -eq 1 ]
-then
-	if [ $user = $usuario ]
-	then
-		if [ $password = $senha ]
-		then
-			echo "<<< Login Autenticado >>>"
-		else
-			echo "Senha nao confere com o sistema"
-		fi
-	else
-		echo "Usuario nao confere com o sistema"
-	fi
-else
-	echo "Campos nao prenchidos"
-	exit 1
-fi
+case $opcao in
+	1)
+		echo "<<< Listar o Diretorio corrente >>>"
+		diretorio=$(ls -C)
+		echo "$diretorio"
+	;;
+	2)
+		echo "<<< Imprimir o nome do diretorio >>>"
+		echo $PWD
+	;;
+	3)
+		echo "<<< Mostrar o usuario logado e o tempo em login >>>"
+		whoami
+	;;
+	4)
+		echo "<<< Tempo de sessao ativa >>>"
+		uptime
+	;;
+	5)
+		echo "<<< Processos em execucao >>>"
+		ps
+	;;
+	*)
+	echo "Opcao Invalida do sistema"
+esac
